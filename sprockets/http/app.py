b9debc05c484e9dbbc3a8de096fc18ea202a0ff9
Cedric Wille <cedricw@aweber.com>
2022-05-06 08:59:17 -0400

    Style as in-line code

--- sprockets/http/app.py
+++ sprockets/http/app.py
@@ -213,8 +213,8 @@ class Application(CallbackManager, web.Application):
     .. rubric:: settings['server']
 
     If the `server` parameter is provided, it will be used for the server
-    header. Otherwise, the server header will default as {service}/{version}
-    or {service}.
+    header. Otherwise, the server header will default as
+    ``{service}/{version}`` or ``{service}``.
 
     """
 
